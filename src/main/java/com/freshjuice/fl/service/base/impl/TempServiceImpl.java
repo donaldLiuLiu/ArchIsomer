6565b63a70575a39a8ff603d7d40d8df9ab0d52b
yangmingzi <yangmingzi@mandofin.com>
2019-09-25 18:34:19 +0800

    暂时

--- src/main/java/com/freshjuice/fl/service/base/impl/TempServiceImpl.java
+++ src/main/java/com/freshjuice/fl/service/base/impl/TempServiceImpl.java
@@ -1,6 +1,7 @@
 package com.freshjuice.fl.service.base.impl;
 
 import com.freshjuice.fl.dao.base.ITempMapper;
+import com.freshjuice.fl.dto.base.Temp;
 import com.freshjuice.fl.service.base.ITempService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,4 +12,23 @@ public class TempServiceImpl implements ITempService {
     private ITempMapper tempMapper;
 
 
+    @Override
+    public void saveTemp(Temp temp) {
+        tempMapper.insertTemp(temp);
+    }
+
+    @Override
+    public void deleteTempById(Long id) {
+        tempMapper.deleteTempById(id);
+    }
+
+    @Override
+    public void updateTemp(Temp temp) {
+        tempMapper.updateTemp(temp);
+    }
+
+    @Override
+    public Temp getTempById(Long id) {
+        return tempMapper.selectTempById(id);
+    }
 }
